7bff54fa505ab76beace6c88780245770638d15c
malborno2020 <manuel.albornoz@zoho.com>
2021-08-30 23:01:59 -0400

    Agregando puts y obteniendo informacion

---
+++ ejercicio1/ejercicio_consola.rb
@@ -0,0 +1,18 @@
+puts ("!Hola Mundo!")
+puts("como estas?")
+
+nombre_usuario = gets.chomp
+
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+
+
+
+puts "que edad tienes?"
+edad = gets.chomp
+puts "#{edad} eso es mucho tiempo"
+
+puts "donde vives?"
+domicilio = gets.chomp #quita el /n que es el entre
+puts "donde queda? #{domicilio}"

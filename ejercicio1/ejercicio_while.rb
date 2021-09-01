883db69534b855268714c03fc9681d3f2c0976be
malborno2020 <manuel.albornoz@zoho.com>
2021-08-31 23:27:37 -0400

    añadiendo ejercicio 2

--- ejercicio1/ejercicio_while.rb
+++ ejercicio1/ejercicio_while.rb
@@ -14,10 +14,14 @@ monto = 0
 
 While ventas[index] != -1
     puts "ingrese ventas"
-    
-    ventas[index] = gets.chomp.to_i
-    suma += ventas[index]
+    monto = gets.chomp.to_i
+    if (monto includes? "a" or monto includes? "b")
+        puts "La venta no es un número"
+    else
+        ventas[index] = monto
+        suma += ventas[index]
+        index += 1
+    end
 end
-puts "las ventas del día son #{ventas[]}"
+puts "las ventas del día son #{ventas}"
 puts "la venta total del día #{suma}"
-

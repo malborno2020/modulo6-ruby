3f39e69bbb87bc5fa7df3c79980e89d743a059f9
malborno2020 <manuel.albornoz@zoho.com>
2021-09-14 10:29:34 -0300

    Corrigiendo metodo atacar monstruos

--- poo/main.rb
+++ poo/main.rb
@@ -1,5 +1,20 @@
 require_relative 'monstruo'
 
+monstruo1 = Monstruo.new
+monstruo1.nombre = "Predator"
+monstruo1.vida = 10
+monstruo1.ataque = 2
+
+monstruo2 = Monstruo.new
+monstruo2.nombre = "Alien"
+monstruo2.vida = 15
+monstruo2.ataque = 9
+
+puts monstruo1
+puts
+puts monstruo2
+
+monstruo1.atacar
 
 
 
--- poo/monstruo.rb
+++ poo/monstruo.rb
@@ -3,26 +3,31 @@ class Monstruo
     attr_accessor :vida
     attr_accessor :ataque
 
+    # def atacar
+    #     while monstruo1.vida > 0 && monstruo2.vida > 0
+    #         quien_ataca = rand(1..2)
+    #         if quien_ataca == 1 
+    #             monstruo2.vida -= monstruo1.ataque
+    #         else
+    #             monstruo1.vida -= monstruo2.ataque
+    #         end
+    #     end
+    #     if monstruo1.vida == 0
+    #         puts "#{monstruo2.nombre} ha aniquilado #{monstruo1.nombre}"
+    #     else
+    #         puts "#{monstruo1.nombre} ha aniquilado #{monstruo2.nombre}"
+    #     end
+    # end
     def atacar
-        while monstruo1.vida > 0 && monstruo2.vida > 0
-            quien_ataca = rand(1..2)
-            if quien_ataca == 1 
+        while monstruo2.vida != 0
                 monstruo2.vida -= monstruo1.ataque
-            else
-                monstruo1.vida -= monstruo2.ataque
-            end
         end
+        puts "#{monstruo1.nombre} ha aniquilado #{monstruo2.nombre}"
+    end
+
+    def to_s
+        puts "Información de #{@nombre}"
+        puts "Cantidad de vida #{@vida}"
+        puts "Capacidad de Ataques #{@ataque}"
     end
 end
-
-monstruo1 = Monstruo.new
-monstruo1.nombre = "Depredador"
-monstruo1.vida = "10"
-monstruo1.ataque = "2"
-
-monstruo2 = Monstruo.new
-monstruo2.nombre = "Alien"
-monstruo2.vida = "15"
-monstruo2.ataque = "9"
-
-
